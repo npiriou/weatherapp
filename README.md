c7bcc7c391e9a4e654ed4bf051d42718c721f222
Nicolas Piriou <n.piriou@yahoo.fr>
2020-12-09 11:34:56 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+For my first time using an API, I made a small weather app using bootstrap to make mobile-first 
+# weatherapp
---
+++ scripts.js
@@ -0,0 +1,27 @@
+let apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=Paris,fr&APPID=a33576c7bfd6ba221202c241d8cb8888&lang=fr'
+async function getWeather() {
+    const response = await fetch(apiUrl)
+    const data = await response.json()
+console.log(data.cod)
+    if (data.cod === '404') document.getElementById('text-404').style.visibility = 'visible'
+    else {
+        document.getElementById('text-404').style.visibility = 'hidden'
+
+        const { description, icon } = data.weather[0]
+        const tempC = Math.round(data.main.temp - 273.15)
+        const name = data.name;
+
+        document.getElementById('description').textContent = description
+        document.getElementById('icon-container').innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}@2x.png" />`
+        document.getElementById('temperature').textContent = `${tempC}°C`
+        document.getElementById('title').textContent = `Quel temps fait il a ${name} ?`
+    }
+}
+getWeather();
+document.getElementById('search-button').addEventListener('click', () => {
+    const input = document.getElementById('input-city').value
+    if (input)
+        apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${input}&APPID=a33576c7bfd6ba221202c241d8cb8888&lang=fr`
+    getWeather();
+})
+
