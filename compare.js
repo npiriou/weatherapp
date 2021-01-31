f11dad155c489535073a7822313d58b47b6b8b7b
npiriou <n.piriou@yahoo.fr>
2021-01-31 12:45:01 +0100

    API key hidden

--- compare.js
+++ compare.js
@@ -1,45 +1,58 @@
 let cities = [null, null];
 async function getWeather(url, num) {
-    const response = await fetch(url)
-    const data = await response.json()
+  const response = await fetch(url);
+  const data = await response.json();
 
-    if (data.cod === '404') document.getElementById('text-404').style.visibility = 'visible'
-    else {
-        document.getElementById(`text-404-${num}`).style.visibility = 'hidden'
-        cities[num] = data;
-        document.getElementById(`text-found-${num}`).textContent = `Ville trouvée : ${data.name}`;
-    }
+  if (data.cod === "404")
+    document.getElementById("text-404").style.visibility = "visible";
+  else {
+    document.getElementById(`text-404-${num}`).style.visibility = "hidden";
+    cities[num] = data;
+    document.getElementById(
+      `text-found-${num}`,
+    ).textContent = `Ville trouvée : ${data.name}`;
+  }
 }
 
 async function inputToCities() {
-    // faire boucle for
+  // faire boucle for
 
-    for (let i = 0; i < document.getElementsByClassName(`input-city`).length; i++) {
-        if (document.getElementsByClassName(`input-city`)[i].value) {
-            let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${document.getElementsByClassName(`input-city`)[i].value}&APPID=a33576c7bfd6ba221202c241d8cb8888&lang=fr`
-            await getWeather(apiUrl, i);
-        }
+  for (
+    let i = 0;
+    i < document.getElementsByClassName(`input-city`).length;
+    i++
+  ) {
+    if (document.getElementsByClassName(`input-city`)[i].value) {
+      let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${
+        document.getElementsByClassName(`input-city`)[i].value
+      }&APPID=${APIKEY}&lang=fr`;
+      await getWeather(apiUrl, i);
     }
+  }
 }
-
 
 async function compare() {
+  await inputToCities();
 
-    await inputToCities();
+  if (cities.every((city) => city)) {
+    const temp0 = Math.round(cities[0].main.temp - 273.15);
+    const temp1 = Math.round(cities[1].main.temp - 273.15);
 
-    if (cities.every(city => city)) {
-        const temp0 = Math.round(cities[0].main.temp - 273.15)
-        const temp1 = Math.round(cities[1].main.temp - 273.15)
-   
-        const diff = temp1 - temp0;
-        const mess = diff == 0 ? `la même température` : diff > 0 ? `${diff} degrés de plus` : `${Math.abs(diff)} degrés de moins`
+    const diff = temp1 - temp0;
+    const mess =
+      diff == 0
+        ? `la même température`
+        : diff > 0
+        ? `${diff} degrés de plus`
+        : `${Math.abs(diff)} degrés de moins`;
 
-
-        document.getElementById('title').textContent = `En ce moment il fait ${mess} à ${cities[1].name}`
-        document.getElementById('description').textContent = `Il fait ${temp0}°C à ${cities[0].name} et ${temp1}°C à ${cities[1].name}.`
-    }
+    document.getElementById(
+      "title",
+    ).textContent = `En ce moment il fait ${mess} à ${cities[1].name}`;
+    document.getElementById(
+      "description",
+    ).textContent = `Il fait ${temp0}°C à ${cities[0].name} et ${temp1}°C à ${cities[1].name}.`;
+  }
 }
 
-document.getElementById(`compare-button`).addEventListener('click', compare)
-
-
+document.getElementById(`compare-button`).addEventListener("click", compare);
--- scripts.js
+++ scripts.js
@@ -1,29 +1,32 @@
-let apiUrl //= 'http://api.openweathermap.org/data/2.5/weather?q=Paris,fr&APPID=a33576c7bfd6ba221202c241d8cb8888&lang=fr'
+let apiUrl;
 let city1, city2;
 
 async function getWeather() {
-    const response = await fetch(apiUrl)
-    const data = await response.json()
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  if (data.cod === "404")
+    document.getElementById("text-404").style.visibility = "visible";
+  else {
+    document.getElementById("text-404").style.visibility = "hidden";
 
-    if (data.cod === '404') document.getElementById('text-404').style.visibility = 'visible'
-    else {
-        document.getElementById('text-404').style.visibility = 'hidden'
+    const { description, icon } = data.weather[0];
+    const tempC = Math.round(data.main.temp - 273.15);
+    const name = data.name;
 
-        const { description, icon } = data.weather[0]
-        const tempC = Math.round(data.main.temp - 273.15)
-        const name = data.name;
-
-        document.getElementById('description').textContent = description
-        document.getElementById('icon-container').innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}@2x.png" />`
-        document.getElementById('temperature').textContent = `${tempC}°C`
-        document.getElementById('title').textContent = `Quel temps fait il à ${name} ?`
-    }
+    document.getElementById("description").textContent = description;
+    document.getElementById(
+      "icon-container",
+    ).innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}@2x.png" />`;
+    document.getElementById("temperature").textContent = `${tempC}°C`;
+    document.getElementById(
+      "title",
+    ).textContent = `Quel temps fait il à ${name} ?`;
+  }
 }
 getWeather();
-document.getElementById('search-button').addEventListener('click', () => {
-    const input = document.getElementById('input-city').value
-    if (input)
-        apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${input}&APPID=a33576c7bfd6ba221202c241d8cb8888&lang=fr`
-    getWeather();
-})
-
+document.getElementById("search-button").addEventListener("click", () => {
+  const input = document.getElementById("input-city").value;
+  if (input)
+    apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${input}&APPID=${APIKEY}&lang=fr`;
+  getWeather();
+});
